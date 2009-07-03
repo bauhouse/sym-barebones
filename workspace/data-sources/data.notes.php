39b8abfcf25899b3e3d1bd2a591b1da69d2f88b0
Stephen Bau <bauhouse@gmail.com>
2009-07-03 07:26:05 -0700

    Add barebones workspace

---
+++ workspace/data-sources/data.notes.php
@@ -0,0 +1,57 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+	
+	Class datasourcenotes extends Datasource{
+		
+		public $dsParamROOTELEMENT = 'notes';
+		public $dsParamORDER = 'desc';
+		public $dsParamLIMIT = '5';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'date';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamINCLUDEDELEMENTS = array(
+				'date',
+				'note'
+		);
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+		
+		public function about(){
+			return array(
+					 'name' => 'Notes',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'website' => 'http://home/sym/fluidgrids',
+							'email' => 'bauhouse@gmail.com'),
+					 'version' => '1.0',
+					 'release-date' => '2009-07-01T14:04:04+00:00');	
+		}
+		
+		public function getSource(){
+			return '3';
+		}
+		
+		public function allowEditorToParse(){
+			return true;
+		}
+		
+		public function grab(&$param_pool){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+				
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}	
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+			return $result;
+		}
+	}
+
---
+++ workspace/data-sources/data.rss_articles.php
@@ -0,0 +1,64 @@
+<?php
+
+	require_once(TOOLKIT . '/class.datasource.php');
+	
+	Class datasourcerss_articles extends Datasource{
+		
+		public $dsParamROOTELEMENT = 'rss-articles';
+		public $dsParamORDER = 'desc';
+		public $dsParamLIMIT = '20';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamSORT = 'date';
+		public $dsParamSTARTPAGE = '1';
+		public $dsParamHTMLENCODE = 'yes';
+		
+		public $dsParamFILTERS = array(
+				'5' => 'yes',
+		);
+		
+		public $dsParamINCLUDEDELEMENTS = array(
+				'title',
+				'body',
+				'date'
+		);
+
+		public function __construct(&$parent, $env=NULL, $process_params=true){
+			parent::__construct($parent, $env, $process_params);
+			$this->_dependencies = array();
+		}
+		
+		public function about(){
+			return array(
+					 'name' => 'RSS Articles',
+					 'author' => array(
+							'name' => 'Stephen Bau',
+							'website' => 'http://home/sym/fluidgrids',
+							'email' => 'bauhouse@gmail.com'),
+					 'version' => '1.0',
+					 'release-date' => '2009-07-01T14:04:41+00:00');	
+		}
+		
+		public function getSource(){
+			return '1';
+		}
+		
+		public function allowEditorToParse(){
+			return true;
+		}
+		
+		public function grab(&$param_pool){
+			$result = new XMLElement($this->dsParamROOTELEMENT);
+				
+			try{
+				include(TOOLKIT . '/data-sources/datasource.section.php');
+			}
+			catch(Exception $e){
+				$result->appendChild(new XMLElement('error', $e->getMessage()));
+				return $result;
+			}	
+
+			if($this->_force_empty_result) $result = $this->emptyXMLSet();
+			return $result;
+		}
+	}
+
